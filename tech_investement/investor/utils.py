bea6c13d829b5d0e2c6235f25df7a86a346a15d1
MugumoPerm <perminusk21@gmail.com>
2024-01-25 18:01:38 -0500

    initiate an investor  app and add some urls file

---
+++ tech_investement/investor/utils.py
@@ -0,0 +1,15 @@
+import uuid
+
+def generate_uuid():
+    # Generate a UUID4
+    full_uuid = uuid.uuid4()
+
+    # Convert the UUID to a string without hyphens
+    short_uuid = str(full_uuid).replace("-", "")
+
+    # Use only the first 12 characters
+    result = short_uuid[:12]
+
+    return result
+
+# print(generate_uuid())
