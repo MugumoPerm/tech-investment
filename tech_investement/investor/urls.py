5c596f8e3bee3ec83a4cf9c34e38543eb95dcf36
MugumoPerm <perminusk21@gmail.com>
2024-01-29 00:21:35 +0300

    moved

--- tech_investement/investor/urls.py
+++ tech_investement/investor/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('adminDashboard', views.adminDashboard, name='adminDashboard'),
     path('', views.login_view, name='login'),
     path('register/', views.register, name='register'),
-    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('reset_password/', views.reset_password, name='reset_password'),
     path('get_chart_data/', views.get_chart_data, name='get_chart_data'),
     ]
 
--- tech_investement/investor/views.py
+++ tech_investement/investor/views.py
@@ -6,28 +6,78 @@ from django.http import HttpResponse, request, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
+# Create your views here.
 
+#Admin
 # @login_required(login_url='login')
 def adminDashboard(request):
     context = {'products': [100, 200, 30, 40, 500]}
     return render(request, 'adminDashboard.html', context)
 
+#users
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def users(request):
+    return render(request, 'users.html')
+
+def user_profile(request):
+    user_transactions = []
+    user_deposit = []
+    user_withdraw = []
+    user_assets = []
+    user_balance = []
+
+    return render(request, 'user_profile.html')
+
+
+
+#authentications
+def adminLogin(request):
+    return render(request, 'adminLogin.html')
+
 def login_view(request):
     return render(request, 'login.html')
+
+def logout_view(request):
+    return render(request, 'logout.html')
 
 def register(request):
     return render(request, 'register.html')
 
 
-def forgot_password(request):
-    return render(request, 'forgot_password.html')
+def reset_password(request):
+    return render(request, 'reset_password.html')
+
+
+
+#transactions
+def transactions_history(request):
+    return render(request, 'transactions_history.html')
+
+def transactions_pending(request):
+    return render(request, 'transactions_pending.html')
+
+def transactions_completed(request):
+    return render(request, 'transactions_completed.html')
+
+def deposit(request):
+    return render(request, 'deposit.html')
+
+def withdraw(request):
+    return render(request, 'withdraw.html')
+
+#assets
+def assets(request):
+    return render(request, 'assets.html')
+
+
 
 
 #ajax requests
 def get_chart_data(request):
     # Replace this with your actual logic to fetch updated data
-    updated_data = [120, 2599, 80, 6, 7000]
-
+    updated_data = [12, 99, 80, 6, 70]
     return JsonResponse({'data': updated_data})
 
 
