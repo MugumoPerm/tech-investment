60746cb447ee8c38bfa4f05a5c86a3612865a65e
MugumoPerm <perminusk21@gmail.com>
2024-01-29 05:35:04 +0300

    modified the reset password urls

--- tech_investement/investor/urls.py
+++ tech_investement/investor/urls.py
@@ -1,6 +1,7 @@
 #urlpatterns
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -8,15 +9,14 @@ urlpatterns = [
     path('user_profile/', views.user_profile, name='user_profile'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('users/', views.users, name='users'),
-    path('get_chart_data/', views.get_chart_data, name='get_chart_data'),
     
     #authentications
     path('', views.login_view, name='login'),
     path('register/', views.register, name='register'),
-    path('reset_password/', views.reset_password, name='reset_password'),
-    path('reset_confirm/', views.reset_confirm, name='reset_confirm'),
-    path('reset_complete/', views.reset_complete, name='reset_complete'),
-    path('reset_done/', views.reset_done, name='reset_done'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='reset_password.html'), name='reset_password'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='reset_complete'),
+    path('reset_done/', auth_views.PasswordResetDoneView.as_view(), name='reset_done'),
     path('logout/', views.logout_view, name='logout'),
     
     #transactions
@@ -32,8 +32,11 @@ urlpatterns = [
     #admin
     path('admin_login/', views.adminLogin, name='admin_login'),
     path('adminDashboard', views.adminDashboard, name='adminDashboard'),
+    path('admin_workplace/', views.admin_workplace, name='admin_workplace'),
     path('admin_logout/', views.admin_logout, name='admin_logout'),
-    path('admin_login/', views.adminLogin, name='admin_login'),
+    path('admin_users/', views.admin_users, name='admin_users'),
+    #charts(ajax)
+    path('get_chart_data/', views.get_chart_data, name='get_chart_data'),
 
     ]
 
